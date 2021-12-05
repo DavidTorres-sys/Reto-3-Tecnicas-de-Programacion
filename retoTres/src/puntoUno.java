7228dab22485de2adabef7b1701f5064b97d7427
DavidTorres-sys <david.torresg@udea.edu.co>
2021-12-05 18:35:25 -0500

    b

--- retoTres/src/puntoUno.java
+++ retoTres/src/puntoUno.java
@@ -6,17 +6,25 @@ public class puntoUno {
   public static void main(String[] args){
     Scanner input = new Scanner (System.in);
 
-    //----------- Punto Uno -------------//
-
+    float  sumaNotas = 0;
+    float  promedioNotas = 0;
     int nArray = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de notas a subir"));
     float [] nota = new float [nArray];
 
     System.out.println("Ingrese la(s) nota(s):");
     for(int i = 0 ;i < nArray; i++) {
-      System.out.print((i + 1) + ". notas:");
+      System.out.print((i + 1) + ". nota:");
       nota [i] = input.nextFloat();
     }
-    System.out.println(Arrays.toString(nota));
+    System.out.println("Notas: " + Arrays.toString(nota));
+
+    for(int k = 0; k < nArray; k++){
+      sumaNotas += nota[k];
+    }
+    System.out.println("Suma notas: " + sumaNotas);
+
+    promedioNotas = sumaNotas / nArray;
+    System.out.println("Promedio Notas: " + promedioNotas);
   }
 }
 
