cff564f3d68e97ee6f2946534c8fd56e8ec9f532
DavidTorres-sys <david.torresg@udea.edu.co>
2021-12-06 16:33:47 -0500

    d

---
+++ retoTres/src/puntoDos.java
@@ -0,0 +1,5 @@
+public class puntoDos {
+  public static void main(String[] args) {
+    System.out.println("hello!");
+  }
+}
--- retoTres/src/puntoUno.java
+++ retoTres/src/puntoUno.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class puntoUno {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
 
     float sumaNotas = 0;
@@ -13,11 +13,11 @@ public class puntoUno {
     int nArray = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de notas a subir"));
     float [] nota = new float [nArray];
 
-    System.out.println("Ingrese la(s) nota(s):");
-    for(int i = 0 ;i < nArray; i++) {
-      System.out.print((i + 1) + ". nota:");
-      nota [i] = input.nextFloat();
-    }
+    System.out.println("Ingrese la(s) nota(s)");
+      for (int i = 0; i < nArray; i++) {
+        System.out.print((i + 1) + ". nota:");
+        nota[i] = input.nextFloat();
+      }
     System.out.println("Notas: " + Arrays.toString(nota));
 
     //-------------- Mayor nota -------------------------//
@@ -27,6 +27,7 @@ public class puntoUno {
       }
     }
     System.out.println("La mayor nota es: " + notaMayor);
+
     //------------ Menor nota ------------------------//
     float notaMenor = nota[0];
     for(int x = 0; x < nArray; x++) {
@@ -35,6 +36,7 @@ public class puntoUno {
       }
     }
     System.out.println("La menor nota es: " + notaMenor);
+
     //------------ Promedio notas -----------------------//
     for(int k = 0; k < nArray; k++) {
       sumaNotas += nota[k];
